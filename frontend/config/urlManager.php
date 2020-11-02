5d7a5d4bb35c350dfa0b333829285633da6f2031
Alexey Vasilyev home <asv2108@gmail.com>
2020-11-02 19:02:57 +0200

    Add urlManager to a single file from the config

---
+++ frontend/config/urlManager.php
@@ -0,0 +1,24 @@
+<?php
+
+/** @var array $params */
+
+if(!isset($params)){
+    $params = require __DIR__ . '/../../common/config/params-local.php';
+}
+
+return [
+    'class' => 'yii\web\UrlManager',
+    'hostInfo' => $params['frontendHostInfo'],
+    'baseUrl' => '',
+    'enablePrettyUrl' => true,
+    'showScriptName' => false,
+    'cache' => false,
+    'rules' => [
+        '' => 'site/index',
+        '<a:login|logout>' => 'site/<_a>',
+        '<_c:[\w\-]+>' => '<_c>/index',
+        '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+        '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
+        '<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_c>/<_a>',
+    ],
+];
