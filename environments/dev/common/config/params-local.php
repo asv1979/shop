3f2a94f7389575b1220a0cfc499404b2d003fe45
Alexey Vasilyev home <asv2108@gmail.com>
2020-11-02 18:28:26 +0200

    Fix return environments

---
+++ environments/dev/common/config/params-local.php
@@ -0,0 +1,8 @@
+<?php
+return [
+'cookieValidationKey' => '',
+'cookieDomain' => '.shop.test',
+'frontendHostInfo' => 'http://shop.test',
+'backendHostInfo' => 'http://backend.shop.test',
+'staticHostInfo' => 'http://static.shop.test',
+];
